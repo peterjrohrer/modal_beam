Unit fixes and attempting some alternate derivatives in simple matrix operations

eigenproblem_santize.py
@@ -13,7 +13,7 @@ class EigenSantize(ExplicitComponent):
         nMode = self.nodal_data['nMode']
 
         self.add_input('Q_mass_norm', val=np.zeros((nDOF_r, nDOF_r)))
-        self.add_input('eigenvals_raw', val=np.zeros((nDOF_r, nDOF_r)), units='1/s')
+        self.add_input('eigenvals_raw', val=np.zeros((nDOF_r, nDOF_r)))
     
         self.add_output('Q_full', val=np.zeros((nDOF_tot, nDOF_r)))
         self.add_output('eig_freqs_full', val=np.zeros(nDOF_r), units='1/s')

eigenvalues.py
@@ -13,10 +13,10 @@ class Eigenvals(om.ExplicitComponent):
         nDOF_r = self.nodal_data['nDOF_r']
         nDOF_tot = self.nodal_data['nDOF_tot']
 
-        self.add_input('Kr_glob', val=np.ones((nDOF_r, nDOF_r)))
+        self.add_input('Kr_glob', val=np.ones((nDOF_r, nDOF_r)), units='N/m')
         self.add_input('Q_mass_norm', val=np.ones((nDOF_r, nDOF_r)))
 
-        self.add_output('eigenvals_raw', val=np.zeros((nDOF_r, nDOF_r)), units='1/s')
+        self.add_output('eigenvals_raw', val=np.zeros((nDOF_r, nDOF_r)))
 
     def compute(self, inputs, outputs):
         nDOF = self.nodal_data['nDOF_r']

eigenvectors_mass_norm.py
@@ -11,10 +11,10 @@ class EigenvecsMassNorm(om.ExplicitComponent):
         self.nodal_data = self.options['nodal_data']
         nDOF_r = self.nodal_data['nDOF_r']
 
-        self.add_input('Mr_glob', val=np.ones((nDOF_r, nDOF_r)))
+        self.add_input('Mr_glob', val=np.ones((nDOF_r, nDOF_r)), units='kg')
         self.add_input('Q_raw', val=np.ones((nDOF_r, nDOF_r)))
 
-        self.add_output('Q_mass_norm', val=np.zeros((nDOF_r, nDOF_r)), units='1/s')
+        self.add_output('Q_mass_norm', val=np.zeros((nDOF_r, nDOF_r)))
 
     def compute(self, inputs, outputs):
         nDOF = self.nodal_data['nDOF_r']

modeshape_M_inv.py
@@ -16,8 +16,8 @@ class ModeshapeMInv(ExplicitComponent):
 
         self.add_output('Mr_glob_inv', val=np.zeros((nDOF, nDOF)), units='1/kg')
 
-    def setup_partials(self):
-        self.declare_partials('Mr_glob_inv', 'Mr_glob')
+    # def setup_partials(self):
+    #     self.declare_partials('Mr_glob_inv', 'Mr_glob')
 
     def compute(self, inputs, outputs):
         M = inputs['Mr_glob']
@@ -27,7 +27,23 @@ class ModeshapeMInv(ExplicitComponent):
 
         outputs['Mr_glob_inv'] = M_inv        
 
-    def compute_partials(self, inputs, partials):
-        M = inputs['Mr_glob']
+    # def compute_partials(self, inputs, partials):
+    #     M = inputs['Mr_glob']
 
-        partials['Mr_glob_inv', 'Mr_glob'] = np.kron(scipy.linalg.inv(M), -1. * scipy.linalg.inv(M).T)
+    #     partials['Mr_glob_inv', 'Mr_glob'] = np.kron(scipy.linalg.inv(M), -1. * scipy.linalg.inv(M).T)
+
+    def compute_jacvec_product(self, inputs, d_inputs, d_outputs, mode):
+        M = inputs['Mr_glob']
+        M_inv = scipy.linalg.inv(M)
+        M_inv[M==0.0] = 0.0
+
+        ## Based on Giles (2008), https://people.maths.ox.ac.uk/gilesm/files/NA-08-01.pdf
+        if mode == 'rev':
+            if 'Mr_glob_inv' in d_outputs:
+                if 'Mr_glob' in d_inputs:
+                    d_inputs['Mr_glob'] += (-1. * M_inv.T) @ d_outputs['Mr_glob_inv'] @ M_inv.T
+
+        elif mode == 'fwd':
+            if 'Mr_glob_inv' in d_outputs:
+                if 'Mr_glob' in d_inputs:
+                    d_outputs['Mr_glob_inv'] += (-1. * M_inv) @ d_inputs['Mr_glob'] @ M_inv

modeshape_eigmatrix.py
@@ -16,7 +16,7 @@ class ModeshapeEigmatrix(ExplicitComponent):
 
         self.add_output('Ar_eig', val=np.zeros((nDOF, nDOF)))
 
-        self.declare_partials('*', '*')
+        # self.declare_partials('*', '*')
 
     def compute(self, inputs, outputs):
         K = inputs['Kr_glob']
@@ -24,11 +24,29 @@ class ModeshapeEigmatrix(ExplicitComponent):
 
         outputs['Ar_eig'] = M_inv @ K
 
-    def compute_partials(self, inputs, partials):
+    # def compute_partials(self, inputs, partials):
+    #     K = inputs['Kr_glob']
+    #     M_inv = inputs['Mr_glob_inv']
+
+    #     N_elem = len(K)
+
+    #     partials['Ar_eig', 'Kr_glob'] = np.kron(M_inv, np.identity(N_elem))
+    #     partials['Ar_eig', 'Mr_glob_inv'] = np.kron(np.identity(N_elem), K.T)
+
+    def compute_jacvec_product(self, inputs, d_inputs, d_outputs, mode):
+        nDOF = self.nodal_data['nDOF_r']
         K = inputs['Kr_glob']
         M_inv = inputs['Mr_glob_inv']
 
-        N_elem = len(K)
+        ## Based on Giles (2008), https://people.maths.ox.ac.uk/gilesm/files/NA-08-01.pdf
+        if mode == 'rev':    
+            if 'Ar_eig' in d_outputs:
+                if 'Mr_glob_inv' in d_inputs:
+                    d_inputs['Mr_glob_inv'] += d_outputs['Ar_eig'] @ K.T
+                if 'Kr_glob' in d_inputs:
+                    d_inputs['Kr_glob'] += M_inv.T @ d_outputs['Ar_eig']
 
-        partials['Ar_eig', 'Kr_glob'] = np.kron(M_inv, np.identity(N_elem))
-        partials['Ar_eig', 'Mr_glob_inv'] = np.kron(np.identity(N_elem), K.T)
+        elif mode == 'fwd':
+            if 'Ar_eig' in d_outputs:
+                if 'Mr_glob_inv' in d_inputs:
+                    d_outputs['Ar_eig'] += (d_inputs['Mr_glob_inv'] @ K) + (M_inv @ d_inputs['Kr_glob'])