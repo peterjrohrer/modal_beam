Some fixes to normalization and reorganizing

eigenvalues.py
@@ -13,8 +13,8 @@ class Eigenvals(om.ExplicitComponent):
         nDOF_r = self.nodal_data['nDOF_r']
         nDOF_tot = self.nodal_data['nDOF_tot']
 
-        self.add_input('Kr_glob', val=np.ones((nDOF_r, nDOF_r)), units='N/m')
-        self.add_input('Q_mass_norm', val=np.ones((nDOF_r, nDOF_r)))
+        self.add_input('Kr_glob', val=np.zeros((nDOF_r, nDOF_r)), units='N/m')
+        self.add_input('Q_mass_norm', val=np.zeros((nDOF_r, nDOF_r)))
 
         self.add_output('eigenvals_raw', val=np.zeros((nDOF_r, nDOF_r)))
 
@@ -23,6 +23,26 @@ class Eigenvals(om.ExplicitComponent):
         K = inputs['Kr_glob']
         Q = inputs['Q_mass_norm']
                 
-        Lambda=np.dot(Q.T,K).dot(Q)
+        Lambda= Q.T @ K @ Q
 
         outputs['eigenvals_raw'] = Lambda
+
+    def compute_jacvec_product(self, inputs, d_inputs, d_outputs, mode):
+        nDOF = self.nodal_data['nDOF_r']
+        K = inputs['Kr_glob']
+        Q = inputs['Q_mass_norm']
+
+        ## Based on Giles (2008), https://people.maths.ox.ac.uk/gilesm/files/NA-08-01.pdf (first quadratic form)
+        if mode == 'rev':    
+            if 'eigenvals_raw' in d_outputs:
+                if 'Q_mass_norm' in d_inputs:
+                    d_inputs['Q_mass_norm'] += (K @ Q @ d_outputs['eigenvals_raw'].T) + (K.T @ Q @ d_outputs['eigenvals_raw']) 
+                if 'Kr_glob' in d_inputs:
+                    d_inputs['Kr_glob'] += Q @ d_outputs['eigenvals_raw'] @ Q.T
+
+        elif mode == 'fwd':
+            if 'eigenvals_raw' in d_outputs:
+                if 'Q_mass_norm' in d_inputs:
+                    d_outputs['eigenvals_raw'] += (d_inputs['Q_mass_norm'].T @ K @ Q) + (Q.T @ K @ d_inputs['Q_mass_norm']) 
+                if 'Kr_glob' in d_inputs:
+                    d_outputs['eigenvals_raw'] += (Q.T @ d_inputs['Kr_glob'] @ Q) 

eigenvectors_mass_norm.py
@@ -11,41 +11,38 @@ class EigenvecsMassNorm(om.ExplicitComponent):
         self.nodal_data = self.options['nodal_data']
         nDOF_r = self.nodal_data['nDOF_r']
 
-        self.add_input('Mr_glob', val=np.ones((nDOF_r, nDOF_r)), units='kg')
-        self.add_input('Q_raw', val=np.ones((nDOF_r, nDOF_r)))
+        self.add_input('M_mode_eig', val=np.zeros((nDOF_r, nDOF_r)), units='kg')
+        self.add_input('Q_raw', val=np.zeros((nDOF_r, nDOF_r)))
 
         self.add_output('Q_mass_norm', val=np.zeros((nDOF_r, nDOF_r)))
 
     def setup_partials(self):
-        self.declare_partials('Q_mass_norm','Mr_glob')
+        self.declare_partials('Q_mass_norm','M_mode_eig')
         self.declare_partials('Q_mass_norm','Q_raw')
         
     def compute(self, inputs, outputs):
         nDOF = self.nodal_data['nDOF_r']
         Q = inputs['Q_raw']
-        M = inputs['Mr_glob']
+        M_modal = inputs['M_mode_eig']
         Q_norm = np.zeros_like(Q)
                 
         for j in range(nDOF):
             q_j = Q[:,j]
-            modalmass_j = np.dot(q_j.T,M).dot(q_j)
-            Q_norm[:,j]= Q[:,j]/np.sqrt(modalmass_j)
-        
-        Q = Q_norm
-        Q_norm = np.zeros_like(Q)
+            modalmass_j = M_modal[j,j]
 
-        for j in range(nDOF):
-            q_j = Q[:,j]
-            iMax = np.argmax(np.abs(q_j))
-            scale = q_j[iMax] # not using abs to normalize to "1" and not "+/-1"
-            Q_norm[:,j]= Q[:,j]/scale
+            # Quick fix to avoid problems with NaN
+            if modalmass_j < 0. :
+                print('[WARN] Found negative modal mass at position %d' %j)
+                modalmass_j = 1.0e-6
+
+            Q_norm[:,j] = Q[:,j]/np.sqrt(modalmass_j)
 
         outputs['Q_mass_norm'] = Q_norm
 
     def compute_partials(self, inputs, partials, discrete_inputs=None):
         nDOF = self.nodal_data['nDOF_r']
         Q = inputs['Q_raw']
-        M = inputs['Mr_glob']
+        M = inputs['M_mode_eig']
 
-        partials['Q_mass_norm', 'Mr_glob'] = np.zeros(((nDOF*nDOF),(nDOF*nDOF)))
+        partials['Q_mass_norm', 'M_mode_eig'] = np.zeros(((nDOF*nDOF),(nDOF*nDOF)))
         partials['Q_mass_norm', 'Q_raw'] = np.zeros(((nDOF*nDOF),(nDOF*nDOF)))